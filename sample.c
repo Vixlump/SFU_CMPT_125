49eb278146e15232d1c8179c9cb28eabe8187185
Vixlump <17090765+Vixlump@users.noreply.github.com>
2022-11-09 22:35:35 -0800

    updated with node helper functions

--- sample.c
+++ sample.c
@@ -6,6 +6,10 @@
 #include "sample.h"
 
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+
 bool contains(const int* a1, int len, const int* a2, int len2) {
   // implement me
   bool is_true;
@@ -36,19 +40,46 @@ int last_len(const char* str) {
   return length;
 }
 
+bool is_leaf(const BTnode_t * node) {
+	return (node->left==NULL&&node->right==NULL);
+}
+bool is_root(const BTnode_t * node) {
+	return (node->parent==NULL);
+}
+
+int get_size(BTnode_t * root) {
+	if (root==NULL) {
+		return 0;
+	} else {
+		return get_size (root->left) + get_size (root->right) +1;
+	}
+}
+int get_depth (BTnode_t * root) {
+	if (root==NULL) {
+		return -1;
+	} else if (is_leaf(root)) {
+		return 0;
+	} else {
+		return MAX(get_depth(root->left), get_depth(root->right))+1;
+	}
+}
+
+void traverse_tree(BTnode_t* root, int * collect_sum) {
+	if (root==NULL) {
+		return;
+	}
+	printf("%d\n", root->value);
+	*collect_sum+=root->value;
+	traverse_tree(root->left, collect_sum);
+	traverse_tree(root->right, collect_sum);
+}
 
 int BT_sum(const BTnode_t* root) {
-  // implement me
-  bool lock = true;
-  int sum = 0;
-  size_t point = &root;
-  while(lock) {
-  	sum += point->value;
-  	if (point==point->right) {
-		lock = false;
-	}
-  	point = &point->right;
-  	
-  }
-  return -1;
+	BTnode_t* temp = create_node(root->value);
+	set_left_child(temp, root->left);
+	set_right_child(temp, root->right);
+	int collect_sum = 0;
+  	traverse_tree(temp, &collect_sum);
+  	printf("Sum:%d\n",collect_sum);
+  	return collect_sum;
 }
